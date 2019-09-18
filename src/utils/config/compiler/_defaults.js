7894a20dde691537651692d744885d8ba4ecfa06
Alejandro Sanchez <aalejo@gmail.com>
2019-09-18 21:07:19 +0000

    added support for "languages" on the bc.json

--- src/utils/config/compiler/_defaults.js
+++ src/utils/config/compiler/_defaults.js
@@ -1,35 +1,41 @@
 module.exports = {
   "html": {
+    language: "html",
     compiler: "webpack",
     tester: "jest",
     ignoreRegex: /.*\.js/gm,
     actions: ['build', 'test']
   },
   "css": {
+    language: "css",
     compiler: "webpack",
     tester: "jest",
     ignoreRegex: /.*\.js/gm,
     actions: ['build', 'test']
   },
   "vanillajs": {
+    language: "vanillajs",
     compiler: "webpack",
     tester: "jest",
     ignoreRegex: null,
     actions: ['build', 'test']
   },
   "react": {
+    language: "react",
     compiler: "webpack",
     tester: "jest",
     ignoreRegex: null,
     actions: ['build', 'test']
   },
   "python3": {
+    language: "python3",
     compiler: "python3",
     tester: "pytest",
     ignoreRegex: null,
     actions: ['run', 'test']
   },
   "node": {
+    language: "node",
     compiler: "node",
     tester: "jest",
     ignoreRegex: null,
